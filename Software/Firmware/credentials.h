28baa0fd38c98d55db091bd44ce36ab4235e9944
R4sp1 <57303554+R4sp1@users.noreply.github.com>
2023-06-21 11:47:50 +0200

    Added WiFi version

--- Software/Firmware/credentials.h
+++ Software/Firmware/credentials.h
@@ -1,1 +1,7 @@
-char nbSERVER[] = "";   // IP address of NB-IoT server
+// NarrowBand IoT credentials
+char nbSERVER[] = "";   // IP address of server used for data upload over NB-IoT
+
+// WiFi credentials
+char SSID[]="";         // WiFi SSID
+char PASSWORD[]="";     // WiFi password
+char wifiSERVER[] = ""; // IP address of server used for data upload over WiFi
